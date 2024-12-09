547edec0042cdbf73f5768a93c51169c1261785b
Almazbek Bazarbekov <yaklass.312@gmail.com>
2024-12-09 21:55:27 +0600

    modification 2

--- Main.java
+++ Main.java
@@ -1,13 +1,37 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Random;
-public class Main
-{
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        int[] boxPositions = scatterBoxes(random);
-        System.out.println("Welcome Martians! Try to guess the locations of your cargo.");
-         while (true) {
+        ArrayList<Integer> boxPositions = scatterBoxes(random);
+        System.out.println("Welcome Martians! Guess where your cargo is hidden.");
+        while (true) {
             boolean allFound = false;
-    }
-}
+            for (int attempt = 1; attempt <= 5; attempt++) {
+                System.out.println("\nAttempt " + attempt + " of 5:");
+                ArrayList<Integer> guesses = new ArrayList<>();
+                for (int i = 0; i < 3; i++) {
+                    System.out.print("Enter guess for box " + (i + 1) + " (1-7): ");
+                    guesses.add(scanner.nextInt());
+                }
+
+                int correct = 0;
+                for (int i = 0; i < 3; i++) {
+                    if (guesses.get(i).equals(boxPositions.get(i))) {
+                        correct++;
+                    }
+                }
+                System.out.println("You guessed " + correct + " boxes correctly!");
+                if (correct == 3) {
+                    System.out.println("Congratulations! All cargo found!");
+                    allFound = true;
+                    break;
+                }
+            }
+            if (allFound) break;
+            System.out.println("\nThe boxes have moved to new locations!");
+            boxPositions = scatterBoxes(random);
+        }
